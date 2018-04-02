d9485a42d45f6fc9528f8ec87a13d7dde74ae9ae
Florian Weber <florian@wurdackweber.de>
2018-04-02 09:51:18 +0200

    added executable for production build removed nodemon because it should be call from the main app refactored www to es6 class wich is then imported in server and can be started from the outside

--- webpack/builder.js
+++ webpack/builder.js
@@ -1,25 +1,29 @@
-const webpack       = require("webpack");
-const clientConfig  = require("./client.config");
-const serverConfig  = require("./server.config");
-const compilers = [];
+import webpack from "webpack";
+import clientConfig from "./client.config";
+import serverConfig from "./server.config";
 
-compilers.push(clientConfig);
-compilers.push(serverConfig);
-
-compilers.map(compiler => {
-   return new Promise((resolve, reject) => {
-      const compile = webpack(compiler);
-
-      compile.run((err, stats) => {
-         if (err) {
-            console.error(err);
-            return reject(err);
-         }
-         resolve(stats);
-      })
-   });
-});
-
-Promise.all(compilers)
-   .then(stats => {
-   });
+export default class Builder{
+    constructor() {
+        this.compilers = [];
+        this.compilers.push(clientConfig);
+        this.compilers.push(serverConfig);
+    }
+    
+    compile() {
+        this.compilers.map(compiler => {
+            return new Promise((resolve, reject) => {
+                const compile = webpack(compiler);
+                
+                compile.run((err, stats) => {
+                    if (err) {
+                        console.error(err);
+                        return reject(err);
+                    }
+                    resolve(stats);
+                })
+            });
+        });
+        
+        return Promise.all(this.compilers);
+    }
+}
