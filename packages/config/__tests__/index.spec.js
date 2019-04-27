3b6d70cec78a3809bddb59c18c4006b02ca9900b
Bastian Wurdack <bastian@wurdackweber.de>
2019-04-27 18:14:09 +0200

    test: added snapshot test to config package

--- packages/config/__tests__/index.spec.js
+++ packages/config/__tests__/index.spec.js
@@ -8,15 +8,11 @@ beforeEach(() => {
 test('should change progressbar option to false', () => {
   const config = getAverjsConfig();
   expect(config.progressbar).toBe(false);
-});
-
-test('should change nested attribute extract to true', () => {
-  const config = getAverjsConfig();
   expect(config.webpack.css.extract).toBe(true);
 });
 
-test('should return a default config when no config file is present', () => {
+test('should match default snapshot config when no config file is present', () => {
   process.env.PROJECT_PATH = '';
   const config = getAverjsConfig();
-  expect(config.progressbar).toBe(true);
+  expect(config).toMatchSnapshot();
 });
