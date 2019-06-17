ef7e77dea70cec5d2feb07b33d0ea4f4a59c8b13
Florian Weber <fl-weber@t-online.de>
2019-06-17 17:06:38 +0200

    refactor(cli): when help or global option is set, it gets executed no matter what

--- packages/cli/lib/index.js
+++ packages/cli/lib/index.js
@@ -22,24 +22,39 @@ export default class Usage {
   }
 
   addCommand(command) {
-    this.availableCommands.push(command);
+    this.availableCommands[command.name] = command;
 
     for (const alias of command.aliases) {
       this.aliases[alias] = command.name;
+
+      // Register aliases for command options
+      for (const argAlias of command.args) {
+        this.aliases[argAlias.alias] = argAlias.name;
+      }
     }
   }
   
   async run() {
-    const executedCommand = this.argv._[0] || 'dev';
-    for (const command of this.availableCommands) {
-      if (executedCommand === command.name) {
-        try {
-          await command.run(this.argv);
-        } catch (err) {
-          console.error(err);
-        }
-        break;
-      }
+    const help = this.argv._[0] === 'help' || this.argv.help;
+    const executedCommand = this.argv._[0] || (!help && 'dev');
+    const commandToExecute = this.availableCommands[executedCommand];
+
+    const globalCommand = this.availableCommands['help'].args.find(option => {
+      return this.argv[option.name];
+    });
+
+    // If a global option is found, execute it and stop afterwards
+    if (globalCommand) {
+      globalCommand.command.run();
+      return;
+    }
+
+    try {
+      // No matter how help is set, do not run the actual command, instead show default help or for specified command
+      if (help) await this.availableCommands['help'].run(executedCommand !== 'help' && commandToExecute);
+      else await commandToExecute.run(this.argv);
+    } catch (err) {
+      console.error(err);
     }
   }
 }
