6e96b29907214ede5ed4e6a05bcc87dec8dc1ff5
Florian Weber <fl-weber@t-online.de>
2019-06-17 17:29:43 +0200

    refactor(cli): add `parseArgs` method which every command inherits

--- packages/cli/lib/commands/command.js
+++ packages/cli/lib/commands/command.js
@@ -12,4 +12,18 @@ export default class Command {
   run() {}
 
   generateCommandLineUsage() {}
+
+  /**
+   * Returns an object which holds all passed arguments from the cli.
+   * TODO: Parse numbers or strings when passed. Currently only return boolean.
+   * @param {*} argv
+   */
+  parseArgs(argv) {
+    return this.args.filter(arg => {
+      return argv[arg.name];
+    }).reduce((prev, cur) => {
+      prev[cur.name] = true;
+      return prev;
+    }, {});
+  }
 }
