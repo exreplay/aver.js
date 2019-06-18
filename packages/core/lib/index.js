d1f0dee2b965b084d78a5220fdad08028c7cfdf5
Florian Weber <fl-weber@t-online.de>
2019-06-18 10:52:43 +0200

    fix(core): changed package so it can be built with rollup

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,7 +1,6 @@
 const path = require('path');
-const requireModule = require('esm')(module);
 
 process.env.PROJECT_PATH = path.resolve(process.cwd(), './src');
 process.env.API_PATH = path.resolve(process.cwd(), './api');
 
-module.exports = requireModule('./core.js').default;
+module.exports = require('./core.js');
