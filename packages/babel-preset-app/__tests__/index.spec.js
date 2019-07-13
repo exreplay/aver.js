11d72efdc5e0bb5080c7d6a1b004f88615cd2949
Florian Weber <fl-weber@t-online.de>
2019-07-13 14:14:52 +0200

    test(babel-preset-app): added tests

---
+++ packages/babel-preset-app/__tests__/index.spec.js
@@ -0,0 +1,74 @@
+import * as babel from '@babel/core';
+import preset from '../lib/index';
+
+const defaultOptions = {
+  babelrc: false,
+  presets: [
+    [ preset, { buildTarget: 'client' } ]
+  ],
+  filename: 'test-entry-file.js'
+};
+
+test('polyfill detection', () => {
+  let { code } = babel.transformSync(`const a = new Map()`.trim(), {
+    ...defaultOptions,
+    ...{
+      presets: [
+        [ preset, { buildTarget: 'server' } ]
+      ]
+    }
+  });
+
+  expect(code).not.toMatch('es.map');
+
+  ;({ code } = babel.transformSync(`const a = new Map()`.trim(), defaultOptions));
+
+  expect(code).toMatch('es.map');
+  expect(code).toMatch('es.promise');
+  expect(code).toMatch('es.array.iterator');
+});
+
+test('dynamic import', () => {
+  expect(() => {
+    babel.transformSync(`const test = () => import('test.vue');`.trim(), defaultOptions);
+  }).not.toThrow();
+});
+
+test('object rest spread', () => {
+  let { code } = babel.transformSync(`
+    const a = { ...b };
+  `.trim(), defaultOptions);
+
+  console.log(code);
+});
+
+test('regenerator runtime should be included on client', () => {
+  let { code } = babel.transformSync(`
+    async function test() {
+      await Promise.resolve();
+    }
+    test();
+  `.trim(), defaultOptions);
+
+  expect(code).toMatch('es.promise');
+  expect(code).toMatch('regenerator-runtime/runtime');
+});
+
+test('decorators should create a _class var', () => {
+  let { code } = babel.transformSync(`
+    function test() {}
+    @test class decoratedClass {}
+  `, defaultOptions);
+
+  expect(code).toMatch('var _class;');
+});
+
+test('class properties should not throw', () => {
+  expect(() => {
+    babel.transformSync(`
+      class decoratedClass {
+        test = '';
+      }
+    `, defaultOptions);
+  }).not.toThrow();
+});
