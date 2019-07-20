0ae1bb34e74d6b9d5a9fb5b9feaf98f521a85736
Florian Weber <fl-weber@t-online.de>
2019-07-20 10:39:34 +0200

    test: only use tests for core-js 2 and added transformFactory

--- packages/babel-preset-app/__tests__/index.spec.js
+++ packages/babel-preset-app/__tests__/index.spec.js
@@ -1,66 +1,73 @@
 import * as babel from '@babel/core';
 import preset from '../lib/index';
+import merge from 'lodash/merge';
 
-const defaultOptions = {
-  babelrc: false,
-  presets: [
-    [ preset, { buildTarget: 'client' } ]
-  ],
-  filename: 'test-entry-file.js'
-};
+function transformFactory(codeToTransform, options) {
+  const defaultOptions = {
+    babelrc: false,
+    presets: [
+      [ preset, { buildTarget: 'client', corejs: 2 } ]
+    ],
+    filename: 'test-entry-file.js'
+  };
 
-test('polyfill detection', () => {
-  let { code } = babel.transformSync(`const a = new Map()`.trim(), {
-    ...defaultOptions,
-    ...{
-      presets: [
-        [ preset, { buildTarget: 'server' } ]
-      ]
-    }
+  return babel.transformSync(codeToTransform, merge(defaultOptions, options));
+}
+
+test('polyfill detection for core-js', () => {
+  let { code } = transformFactory(`const a = new Map()`, {
+    presets: [
+      [ preset, { buildTarget: 'server' } ]
+    ]
   });
 
-  expect(code).not.toMatch('es.map');
+  expect(code).toMatch('runtime-corejs2/core-js/map');
 
-  ;({ code } = babel.transformSync(`const a = new Map()`.trim(), defaultOptions));
+  ;({ code } = transformFactory(`const a = new Map()`.trim()));
 
-  expect(code).toMatch('es.map');
-  expect(code).toMatch('es.promise');
-  expect(code).toMatch('es.array.iterator');
+  expect(code).toMatch('runtime-corejs2/core-js/map');
+  expect(code).toMatch('es6.promise');
+  expect(code).toMatch('es6.array.iterator');
 });
 
 test('dynamic import', () => {
   expect(() => {
-    babel.transformSync(`const test = () => import('test.vue');`.trim(), defaultOptions);
+    transformFactory(`const test = () => import('test.vue');`);
   }).not.toThrow();
 });
 
+test('rest spread should use assign polyfill', () => {
+  let { code } = transformFactory(`const a = { ...b };`);
+  expect(code).toMatch('@babel/runtime-corejs2/core-js/object/assign');
+});
+
 test('regenerator runtime should be included on client', () => {
-  let { code } = babel.transformSync(`
+  let { code } = transformFactory(`
     async function test() {
       await Promise.resolve();
     }
     test();
-  `.trim(), defaultOptions);
+  `.trim());
 
-  expect(code).toMatch('es.promise');
+  expect(code).toMatch('es6.promise');
   expect(code).toMatch('regenerator-runtime/runtime');
 });
 
 test('decorators should create a _class var', () => {
-  let { code } = babel.transformSync(`
+  let { code } = transformFactory(`
     function test() {}
     @test class decoratedClass {}
-  `, defaultOptions);
+  `);
 
   expect(code).toMatch('var _class;');
 });
 
 test('class properties should not throw', () => {
   expect(() => {
-    babel.transformSync(`
+    transformFactory(`
       class decoratedClass {
         test = '';
       }
-    `, defaultOptions);
+    `);
   }).not.toThrow();
 });
