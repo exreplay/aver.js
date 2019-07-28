632a1ea4ebbeb4584fa7a493582f10f262f5783b
Florian Weber <fl-weber@t-online.de>
2019-07-28 11:10:18 +0200

    refactor(cli): set `PROJECT_PATH` and `API_PATH`

--- packages/cli/bin/aver.js
+++ packages/cli/bin/aver.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 
+const path = require('path');
 const requireModule = require('esm')(module);
+
+process.env.PROJECT_PATH = path.resolve(process.cwd(), './src');
+process.env.API_PATH = path.resolve(process.cwd(), './api');
+
 const Cli = requireModule('../lib').default;
 const cli = new Cli();
 cli.run();
