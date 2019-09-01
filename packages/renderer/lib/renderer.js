b47ae0cd32d8071c60b2285fff48b145a7bef1be
Florian Weber <fl-weber@t-online.de>
2019-09-01 10:23:11 +0200

    feat: vuex store config (#95)
    * refactor(config): add store config param to vue-app
    
    * refactor(renderer): pass store config to lodash template compiler
    
    * refactor(vue-app): merge config from aver-config.js and add support for mixin file
    
    * refactor(vue-app): call mixin as function instead of make a new instance

--- packages/renderer/lib/renderer.js
+++ packages/renderer/lib/renderer.js
@@ -52,7 +52,8 @@ export default class Renderer {
             progressbar: this.globalConfig.progressbar,
             i18n: this.globalConfig.i18n,
             csrf: this.globalConfig.csrf,
-            router: this.globalConfig.router
+            router: this.globalConfig.router,
+            store: this.globalConfig.store
           }
         });
 
