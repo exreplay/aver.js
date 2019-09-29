8cc9d125c53521511426b055727fbe49883ba95f
Florian Weber <fl-weber@t-online.de>
2019-09-29 09:35:16 +0200

    refactor(vuex): only require files on first level, support default vuex syntax and throw warning when multiple files are trying to set the global vuex store

--- packages/vue-app/lib/store/index.js
+++ packages/vue-app/lib/store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import forEach from 'lodash/forEach';
 import { ExportVuexStore } from '@averjs/vuex-decorators';
 import createPersistedState from 'vuex-persistedstate';
 import * as Cookies from 'js-cookie';
@@ -9,25 +8,52 @@ import merge from 'lodash/merge'
 Vue.use(Vuex);
 
 export function createStore(ssrContext) {
-  const files = require.context('@/', true, /vuex\/[^.]+\.js$/i);
+  const files = require.context('@/', true, /vuex\/([^/]+)\.js$/i);
   const modules = {};
   const persistent = [];
   const plugins = [];
+  let defaultConfig = {};
+  let globalStoreSet = false;
+  let ignoredGlobalStores = [];
 
-  forEach(files.keys(), r => {
-    const storeFile = ExportVuexStore(files(r).default);
-    if (typeof storeFile.moduleName !== 'undefined') {
-      modules[storeFile.moduleName] = storeFile;
-        
-      if (storeFile.persistent && typeof storeFile.persistent === 'boolean') {
-        persistent.push(storeFile.moduleName);
-      } else if (storeFile.persistent && typeof storeFile.persistent === 'object') {
-        forEach(storeFile.persistent, state => {
-          persistent.push(storeFile.moduleName + '.' + state);
-        });
+  for(const r of files.keys()) {
+    const store = files(r).default;
+
+    if(typeof store === 'function') {
+      const storeFile = ExportVuexStore(store);
+      if (typeof storeFile.moduleName !== 'undefined') {
+        modules[storeFile.moduleName] = storeFile;
+          
+        if (storeFile.persistent && typeof storeFile.persistent === 'boolean') {
+          persistent.push(storeFile.moduleName);
+        } else if (storeFile.persistent && typeof storeFile.persistent === 'object') {
+          for(const state of storeFile.persistent) {
+            persistent.push(storeFile.moduleName + '.' + state);
+          };
+        }
+      }
+    } else if(typeof store === 'object' && store.moduleName) {
+      modules[store.moduleName] = store;
+    } else if(typeof store === 'object') {
+      if(!globalStoreSet) {
+        // Copy object, otherwise it is not mutable
+        const globalStore = { ...store };
+        if(globalStore.namespaced) delete globalStore.namespaced;
+        defaultConfig = { ...globalStore };
+        globalStoreSet = r;
+      } else {
+        ignoredGlobalStores.push(r);
       }
     }
-  });
+  }
+
+  if(ignoredGlobalStores.length > 0 && process.env.NODE_ENV === 'development') {
+    console.warn(chalk.red(`
+We found multiple files which try to set the global store. Be aware that there can only be 1 global store file.
+The file '${globalStoreSet}' was used.
+The following files have been ignored:${ignoredGlobalStores.reduce((prev, cur) => prev += `, '${cur}'`, '').substring(1)}.
+    `));
+  }
 
   if (persistent.length > 0) {
     plugins.push(
@@ -45,7 +71,7 @@ export function createStore(ssrContext) {
     );
   }
 
-  let defaultConfig = { modules, plugins };
+  defaultConfig = { ...defaultConfig, modules, plugins };
   <% if (typeof config.store === 'object') print('defaultConfig = merge(defaultConfig, JSON.parse(\''+JSON.stringify(config.store)+'\'))'); %>
 
   const mixinContext = require.context('@/', false, /^\.\/store\.js$/i);
@@ -62,13 +88,13 @@ export function createStore(ssrContext) {
   if (module.hot) {
     files.keys().map(path => files(path));
     module.hot.accept(files.id, () => {
-      const newFiles = require.context('@/', true, /vuex\/[^.]+\.js$/i);
+      const newFiles = require.context('@/', true, /vuex\/([^/]+)\.js$/i);
       const newModules = {};
 
-      newFiles.keys().forEach(r => {
+      for(const r of newFiles.keys()) {
         const storeFile = ExportVuexStore(newFiles(r).default);
         newModules[storeFile.moduleName] = storeFile;
-      });
+      }
 
       store.hotUpdate({
         modules: newModules
@@ -84,3 +110,4 @@ export function createStore(ssrContext) {
 
   return store;
 }
+
