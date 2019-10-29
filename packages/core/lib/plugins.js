75d9057bdd47288e52db96a93be7aba9709e06db
Florian Weber <fl-weber@t-online.de>
2019-10-29 14:14:15 +0100

    refactor(core): added better resolving for plugins

--- packages/core/lib/plugins.js
+++ packages/core/lib/plugins.js
@@ -1,3 +1,6 @@
+import path from 'path';
+import fs from 'fs';
+
 export default class PluginContainer {
   constructor(aver) {
     this.aver = aver;
@@ -13,11 +16,64 @@ export default class PluginContainer {
     }, Promise.resolve());
   }
 
-  addModule(plugin) {
+  async addModule(plugin) {
+    let src;
+    let options;
+    let handler;
+
+    if (typeof plugin === 'string') {
+      src = plugin;
+    } else if (typeof plugin === 'function') {
+      handler = plugin;
+    } else if (Array.isArray(plugin)) {
+      [ src, options ] = plugin;
+    }
+
+    if (!handler) {
+      handler = this.require(src);
+    }
+
+    if (typeof handler !== 'function') {
+      throw new Error('Plugin should export a function.');
+    }
+
+    if (!options) {
+      options = {};
+    }
+
+    await handler.call(this, options);
+  }
+
+  require(src) {
     const requireModule = require('esm')(module);
-    
-    if (typeof plugin === 'string') requireModule(plugin).default.call(this, {});
-    else if (Array.isArray(plugin)) requireModule(plugin[0]).default.call(this, plugin[1] || {});
-    else if (typeof plugin === 'function') plugin.call(this, {});
+    let pluginPath;
+
+    pluginPath = this.resolveModule(src);
+
+    if (!pluginPath) {
+      pluginPath = this.resolvePath(src);
+    }
+
+    if (!pluginPath) {
+      throw new Error(`Could not resolve plugin '${src}'. Please make sure either the package is installed or the file exists.`);
+    }
+
+    return requireModule(pluginPath).default;
+  }
+
+  resolveModule(plugin) {
+    try {
+      return require.resolve(plugin);
+    } catch (err) {
+      if (err.code !== 'MODULE_NOT_FOUND') {
+        throw err;
+      }
+    }
+  }
+
+  resolvePath(plugin) {
+    const pluginPath = path.resolve(process.env.PROJECT_PATH, '../', plugin);
+    if (fs.existsSync(pluginPath)) return pluginPath;
+    else return undefined;
   }
 }
