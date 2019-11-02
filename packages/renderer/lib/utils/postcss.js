a98bd53b76f76ac284c9c46d4d71f655333d26a3
Florian Weber <fl-weber@t-online.de>
2019-11-02 08:58:00 +0100

    refactor(postcss): removed file support, added resolver for postcss-import and refactored class

--- packages/renderer/lib/utils/postcss.js
+++ packages/renderer/lib/utils/postcss.js
@@ -1,58 +1,63 @@
-import fs from 'fs';
 import path from 'path';
 import SafeParser from 'postcss-safe-parser';
+import {
+  NodeJsInputFileSystem,
+  CachedInputFileSystem,
+  ResolverFactory
+} from 'enhanced-resolve';
 
 export default class PostCSS {
   constructor(config) {
     this.config = config;
-    this.postcssConfigExists = false;
-    this.findPostcssConfig();
+    this.isProd = process.env.NODE_ENV === 'production';
   }
 
-  findPostcssConfig() {
-    const files = [
-      '.postcssrc',
-      '.postcssrc.js',
-      'postcss.config.js',
-      '.postcssrc.yaml',
-      '.postcssrc.json'
-    ];
-    const pkg = require(path.resolve(process.cwd(), './package.json'));
+  get postCssConfig() {
+    return {
+      sourceMap: !this.isProd,
+      plugins: this.plugins
+    };
+  }
 
-    if (pkg.postcss) {
-      this.postcssConfigExists = true;
-      return;
-    }
+  plugins() {
+    const plugins = [];
 
-    for (const file of files) {
-      if (fs.existsSync(path.resolve(process.env.PROJECT_PATH, `../${file}`))) {
-        this.postcssConfigExists = true;
-        break;
-      }
-    }
+    plugins.push(
+      require('postcss-import')({
+        resolve: this.resolveImports
+      })
+    );
+
+    plugins.push(...this.config.postcss.plugins);
+
+    plugins.push(
+      require('cssnano')({
+        parser: SafeParser,
+        discardComments: { removeAll: true }
+      })
+    );
+
+    return plugins;
+  }
+
+  resolveImports(id, basedir) {
+    const options = {
+      alias: {
+        '@': path.join(process.env.PROJECT_PATH)
+      },
+      fileSystem: new CachedInputFileSystem(new NodeJsInputFileSystem(), 4000),
+      extensions: [ '.css' ],
+      useSyncFileSystemCalls: true
+    };
+    const resolver = ResolverFactory.createResolver(options);
+
+    return resolver.resolveSync({}, basedir, id);
   }
 
   apply(rule) {
-    if (this.stylesAreInline) {
-      rule
-        .use('cssnano')
-          .loader('postcss-loader')
-          .options({
-            sourceMap: !this.isProd,
-            plugins: [
-              require('cssnano')({
-                parser: SafeParser,
-                discardComments: { removeAll: true }
-              })
-            ]
-          });
-    }
-
-    if (this.postcssConfigExists) {
-      rule
-        .use('postcss')
-          .loader('postcss-loader')
-          .options({ sourceMap: !this.isProd });
-    }
+    rule
+      .use('postcss')
+        .loader('postcss-loader')
+        .options(this.postCssConfig);
   }
 }
