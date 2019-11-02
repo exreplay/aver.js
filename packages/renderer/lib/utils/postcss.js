fb3e91f6be1e2d276293adbc80dfabf9bb1c587c
Florian Weber <fl-weber@t-online.de>
2019-11-02 11:17:04 +0100

    refactor(postcss): changed postCssConfig to defaultConfig, merge it with user config and load plugins afterwards in correct order

--- packages/renderer/lib/utils/postcss.js
+++ packages/renderer/lib/utils/postcss.js
@@ -1,5 +1,5 @@
-import path from 'path';
 import SafeParser from 'postcss-safe-parser';
+import merge from 'lodash/merge';
 import {
   NodeJsInputFileSystem,
   CachedInputFileSystem,
@@ -12,39 +12,30 @@ export default class PostCSS {
     this.isProd = process.env.NODE_ENV === 'production';
   }
 
-  get postCssConfig() {
+  get defaultConfig() {
     return {
       sourceMap: !this.isProd,
-      plugins: this.plugins
+      plugins: {
+        'postcss-import': {
+          resolve: this.resolveImports.bind(this)
+        },
+        cssnano: {
+          parser: SafeParser,
+          discardComments: { removeAll: true }
+        }
+      }
     };
   }
 
-  plugins() {
-    const plugins = [];
-
-    plugins.push(
-      require('postcss-import')({
-        resolve: this.resolveImports
-      })
-    );
-
-    plugins.push(...this.config.postcss.plugins);
-
-    plugins.push(
-      require('cssnano')({
-        parser: SafeParser,
-        discardComments: { removeAll: true }
-      })
-    );
-
-    return plugins;
+  loadPlugins(config) {
+    // ensure cssnano comes last
+    const sortedPluginsKeys = Object.keys(config.plugins).sort(a => a === 'cssnano');
+    config.plugins = sortedPluginsKeys.map(p => require(p)(config.plugins[p]));
   }
 
   resolveImports(id, basedir) {
     const options = {
-      alias: {
-        '@': path.join(process.env.PROJECT_PATH)
-      },
+      alias: this.config.alias,
       fileSystem: new CachedInputFileSystem(new NodeJsInputFileSystem(), 4000),
       extensions: [ '.css' ],
       useSyncFileSystemCalls: true
@@ -55,9 +46,12 @@ export default class PostCSS {
   }
 
   apply(rule) {
+    const config = merge({}, this.defaultConfig, this.config.postcss);
+    this.loadPlugins(config);
+
     rule
       .use('postcss')
         .loader('postcss-loader')
-        .options(this.postCssConfig);
+        .options(config);
   }
 }
