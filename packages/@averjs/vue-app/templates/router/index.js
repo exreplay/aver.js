2ab527f9997baef937447253dd9c70628f6fb849
Florian Weber <fl-weber@t-online.de>
2020-02-01 08:12:16 +0100

    linting

--- packages/@averjs/vue-app/templates/router/index.js
+++ packages/@averjs/vue-app/templates/router/index.js
@@ -6,14 +6,14 @@ Vue.use(VueRouter);
 Vue.use(Meta);
 
 export function createRouter({ i18n }) {
-  const routes = require('@/pages').default;
-  let config = {
-    mode: 'history',
-    fallback: false
-  };
+  const routes = require('@/pages').default;
+  let config = {
+    mode: 'history',
+    fallback: false
+  };
 
-  if (Array.isArray(routes)) config = { ...config, routes };
-  else if (typeof routes === 'function') config = routes({ i18n, config });
+  if (Array.isArray(routes)) config = { ...config, routes };
+  else if (typeof routes === 'function') config = routes({ i18n, config });
 
-  return new VueRouter(config);
+  return new VueRouter(config);
 }
