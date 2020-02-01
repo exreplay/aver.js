424d3c7ed7d68ddf7771f16dce3d7c68aabe03f4
Florian Weber <fl-weber@t-online.de>
2020-02-01 09:13:34 +0100

    feat: moved i18n config to its own file with support for mixins

---
+++ packages/@averjs/vue-app/templates/i18n.js
@@ -0,0 +1,41 @@
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import * as Cookies from 'js-cookie';
+import merge from 'lodash/merge';
+
+Vue.use(VueI18n);
+
+export function createI18n(ssrContext) {
+  let i18nConfig = {
+    locale: 'de',
+    fallbackLocale: 'de'
+  };
+
+  <% if(typeof config.i18n !== 'undefined') print('i18nConfig = Object.assign(i18nConfig, JSON.parse(\''+JSON.stringify(config.i18n)+'\'));') %>
+
+  const mixinContext = require.context('@/', false, /^\.\/i18n\.js$/i);
+  for (const r of mixinContext.keys()) {
+    const mixin = mixinContext(r).default;
+    if (typeof mixin !== 'undefined') {
+      const mixinConfig = mixin(i18nConfig);
+      i18nConfig = merge(i18nConfig, mixinConfig);
+    }
+  }
+
+  const i18n = new VueI18n(i18nConfig);
+
+  if (!ssrContext.isServer) i18n.locale = Cookies.get('language') || i18nConfig.locale;
+  else i18n.locale = ssrContext.context.cookies.language || i18nConfig.locale;
+
+  Vue.prototype.$locale = {
+    change: (lang) => {
+      i18n.locale = lang;
+      Cookies.set('language', i18n.locale, { secure: process.env.NODE_ENV === 'production' });
+    },
+    current: () => {
+      return i18n.locale;
+    }
+  };
+
+  return i18n;
+}
