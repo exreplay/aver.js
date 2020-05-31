2e465179a8fd11a97c0116f2dea7ee2e8c38ce90
Florian Weber <fl-weber@t-online.de>
2020-05-31 16:51:22 +0200

    refactor: not initialize redis store when no env variables are defined

--- packages/@averjs/session/index.js
+++ packages/@averjs/session/index.js
@@ -6,29 +6,32 @@ import merge from 'lodash/merge';
 
 export default function(options) {
   if (process.argv.indexOf('build') !== -1) return;
-  
+
   const {
     redisStoreConfig = {},
     expressSessionConfig = {},
     ttl = 60 * 60
   } = options;
+  let store = null;
 
-  const RedisStore = ConnectRedis(session);
-  const client = new Redis(process.env.REDIS_PORT, process.env.REDIS_HOST, {
-    password: process.env.REDIS_PASSWORD
-  });
-  const store = new RedisStore({
-    client,
-    prefix: 'sess-' + new Date().getDate() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getFullYear() + ':',
-    ttl,
-    ...redisStoreConfig
-  });
+  if (!process.env.REDIS_PORT && !process.env.REDIS_HOST && !process.env.REDIS_PASSWORD) {
+    const RedisStore = ConnectRedis(session);
+    const client = new Redis(process.env.REDIS_PORT, process.env.REDIS_HOST, {
+      password: process.env.REDIS_PASSWORD
+    });
+    store = new RedisStore({
+      client,
+      prefix: 'sess-' + new Date().getDate() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getFullYear() + ':',
+      ttl,
+      ...redisStoreConfig
+    });
+  }
 
   const config = merge({
     secret: process.env.REDIS_SECRET || uuidv4(),
     genid: () => uuidv4(),
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     cookie: {
       expires: new Date(Date.now() + (ttl * 1000)),
       maxAge: ttl * 1000,
