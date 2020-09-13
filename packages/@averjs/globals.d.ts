71701d7d1d6aec7ea2898b9bde9cd0c673f50b22
Florian Weber <fl-weber@t-online.de>
2020-09-13 13:46:38 +0200

    fix: added back api and project path

--- packages/@averjs/globals.d.ts
+++ packages/@averjs/globals.d.ts
@@ -1,5 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     NODE_ENV: 'production' | 'development' | 'test' | undefined;
+    API_PATH: string;
+    PROJECT_PATH: string;
   }
 }
