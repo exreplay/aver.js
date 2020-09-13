e6801ea228853ed31cbf90d7675ee3c0f477a181
Florian Weber <fl-weber@t-online.de>
2020-09-13 11:21:49 +0200

    fix: added version alias

--- packages/@averjs/cli/lib/commands/version.ts
+++ packages/@averjs/cli/lib/commands/version.ts
@@ -3,6 +3,7 @@ import { version } from '@averjs/cli/package.json';
 
 export default class VersionCommand extends Command implements CommandInterface {
   name = 'version';
+  alias = 'v';
   description = 'Show the current installed aver.js version.';
   type = Boolean;
 
