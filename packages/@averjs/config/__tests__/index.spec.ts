83e7a1815e85c14da3e87009873ee6ec755b1c3c
Florian Weber <fl-weber@t-online.de>
2020-09-13 11:37:28 +0200

    refactor: rewrite test to ts

--- packages/@averjs/config/__tests__/index.spec.ts
+++ packages/@averjs/config/__tests__/index.spec.ts
@@ -2,13 +2,17 @@ import path from 'path';
 import { getAverjsConfig } from '../lib/index';
 
 beforeEach(() => {
+  console.log = jest.fn();
+});
+
+beforeEach(() => {
   process.env.PROJECT_PATH = path.resolve(__dirname, '../__fixtures__/src');
 });
 
 test('should change progressbar option to false', () => {
   const config = getAverjsConfig();
   expect(config.progressbar).toBe(false);
-  expect(config.webpack.css.extract).toBe(true);
+  expect(config.webpack.css?.extract).toBe(true);
 });
 
 test('should concat array with default values', () => {
@@ -18,11 +22,11 @@ test('should concat array with default values', () => {
 });
 
 test('should match default snapshot config when no config file is present', () => {
-  jest.spyOn(process, 'cwd').mockReturnValue('/');
+  const processSpy = jest.spyOn(process, 'cwd').mockReturnValue('/');
   
   process.env.PROJECT_PATH = '/aver';
   const config = getAverjsConfig();
   expect(config).toMatchSnapshot();
 
-  process.cwd.mockRestore();
+  processSpy.mockRestore();
 });
