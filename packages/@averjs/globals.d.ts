4319000188d939f530f345136ef4de5f4dd5f7d6
Florian Weber <fl-weber@t-online.de>
2020-09-19 10:27:19 +0200

    fix: updated globals

--- packages/@averjs/globals.d.ts
+++ packages/@averjs/globals.d.ts
@@ -3,5 +3,9 @@ declare namespace NodeJS {
     NODE_ENV: 'production' | 'development' | 'test' | undefined;
     API_PATH: string;
     PROJECT_PATH: string;
+
+    REDIS_PORT: string;
+    REDIS_HOST: string;
+    REDIS_PASSWORD: string;
   }
 }
