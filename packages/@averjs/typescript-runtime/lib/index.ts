433fb25d725fe6a80cb1eaf476ed6d5621f9a8ae
Florian Weber <fl-weber@t-online.de>
2020-10-03 21:16:11 +0200

    feat: added support for updating eslint config

--- packages/@averjs/typescript-runtime/lib/index.ts
+++ packages/@averjs/typescript-runtime/lib/index.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'path';
-import { existsSync } from 'fs';
+import { existsSync, writeFileSync, readFileSync } from 'fs';
 import { copy } from 'fs-extra';
 import { register } from 'ts-node';
 import parseArgs, { ParsedArgs } from 'minimist';
@@ -12,6 +12,10 @@ interface AvailableCommands extends ParsedArgs {
 
 export default class CLI {
   argv: AvailableCommands = parseArgs(process.argv.slice(2));
+
+  get tsConfigPath() { return resolve(process.env.PROJECT_PATH, '../tsconfig.json'); }
+  get tsConfigServerPath() { return resolve(process.env.PROJECT_PATH, '../tsconfig.server.json'); }
+  get eslintrcPath() { return resolve(process.env.PROJECT_PATH, '../.eslintrc.js'); }
   
   async run() {
     if(this.argv.init) {
@@ -20,7 +24,7 @@ export default class CLI {
     }
 
     register({
-      project: resolve(process.env.PROJECT_PATH, '../tsconfig.server.json')
+      project: this.tsConfigServerPath
     });
 
     const avercli = new AverCli();
@@ -29,10 +33,10 @@ export default class CLI {
 
   async init() {
     const tsConfigSpinner = ora('Copying tsconfig.json').start();
-    if (!existsSync(resolve(process.env.PROJECT_PATH, '../tsconfig.json'))) {
+    if (!existsSync(this.tsConfigPath)) {
       await copy(
         resolve(__dirname, '../templates/tsconfig.json'),
-        resolve(process.env.PROJECT_PATH, '../tsconfig.json')
+        this.tsConfigPath
       );
       tsConfigSpinner.succeed();
     } else {
@@ -40,14 +44,30 @@ export default class CLI {
     }
 
     const tsConfigServerSpinner = ora('Copying tsconfig.server.json').start();
-    if (!existsSync(resolve(process.env.PROJECT_PATH, '../tsconfig.server.json'))) {
+    if (!existsSync(this.tsConfigServerPath)) {
       await copy(
         resolve(__dirname, '../templates/tsconfig.server.json'),
-        resolve(process.env.PROJECT_PATH, '../tsconfig.server.json')
+        this.tsConfigServerPath
       );
       tsConfigServerSpinner.succeed();
     } else {
       tsConfigServerSpinner.info('Skipping because tsconfig.server.json already exists.');
+    }
+
+    const eslintrcSpinner = ora('Updating eslintrc').start();
+    const eslintrcPath = this.eslintrcPath;
+    const eslintrc = await import(eslintrcPath);
+    if(!eslintrc.extends?.find((e: string) => e === '@averjs/typescript/eslint')) {
+      const content = readFileSync(eslintrcPath, 'utf-8');
+      const lines = content.split('\n');
+      for(const i in lines) {
+        const line = lines[i];
+        if(line.includes('extends')) lines.splice(parseInt(i) + 1, 0, `    '@averjs/typescript/eslint',`);
+      }
+      writeFileSync(eslintrcPath, lines.join('\n'));
+      eslintrcSpinner.succeed();
+    } else {
+      eslintrcSpinner.info('Skipping because plugin already exists.');
     }
   }
 }
