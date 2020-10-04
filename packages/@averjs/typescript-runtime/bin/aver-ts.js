083aba0a0b7f9851adf9825aedd6717811e4a3bf
Florian Weber <fl-weber@t-online.de>
2020-10-04 14:47:28 +0200

    fix: resgister ts-node before executing runtime

--- packages/@averjs/typescript-runtime/bin/aver-ts.js
+++ packages/@averjs/typescript-runtime/bin/aver-ts.js
@@ -6,6 +6,12 @@ const path = require('path');
 process.env.PROJECT_PATH = path.resolve(process.cwd(), './src');
 process.env.API_PATH = path.resolve(process.cwd(), './api');
 
+const tsNode = require('ts-node');
+tsNode.register({
+  project: typescriptInitCommand.tsConfigServerPath
+});
+
+
 const RuntimeCli = require('../dist/typescript-runtime');
 const cli = new RuntimeCli();
 cli.run();
