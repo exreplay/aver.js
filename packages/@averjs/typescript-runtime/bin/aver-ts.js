098e322afde9e9ef9fb635712e202852a0266e0d
Florian Weber <fl-weber@t-online.de>
2020-10-04 14:59:47 +0200

    fix: use correct path for project (#262)

--- packages/@averjs/typescript-runtime/bin/aver-ts.js
+++ packages/@averjs/typescript-runtime/bin/aver-ts.js
@@ -6,11 +6,10 @@ const path = require('path');
 process.env.PROJECT_PATH = path.resolve(process.cwd(), './src');
 process.env.API_PATH = path.resolve(process.cwd(), './api');
 
-const tsNode = require('ts-node');
-tsNode.register({
-  project: typescriptInitCommand.tsConfigServerPath
+const t = require('ts-node');
+t.register({
+  project: path.resolve(process.env.PROJECT_PATH, '../tsconfig.server.json')
 });
-
 
 const RuntimeCli = require('../dist/typescript-runtime');
 const cli = new RuntimeCli();
