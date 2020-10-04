cd842674cfadd9786fb2ad5e83656615e2a66c70
Florian Weber <fl-weber@t-online.de>
2020-10-04 14:48:25 +0200

    fix: resgister ts-node before executing runtime (#261)

--- packages/@averjs/typescript-runtime/lib/index.ts
+++ packages/@averjs/typescript-runtime/lib/index.ts
@@ -1,4 +1,3 @@
-import { register } from 'ts-node';
 import parseArgs, { ParsedArgs } from 'minimist';
 import AverCli from '@averjs/cli';
 import TypescriptInitCommand from './ts-init';
@@ -11,14 +10,8 @@ export default class CLI {
   argv: AvailableCommands = parseArgs(process.argv.slice(2));
   
   async run() {
-    const typescriptInitCommand = new TypescriptInitCommand();
-
-    register({
-      project: typescriptInitCommand.tsConfigServerPath
-    });
-
     const avercli = new AverCli();
-    avercli.addCommand(typescriptInitCommand);
+    avercli.addCommand(new TypescriptInitCommand());
     avercli.run();
   }
 }
