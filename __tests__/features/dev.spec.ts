153b1fdddc7a38194b482e3615fbd66c16e784b2
Florian Weber <fl-weber@t-online.de>
2020-11-14 11:03:00 +0100

    refactor: added feature test for dev build

---
+++ __tests__/features/dev.spec.ts
@@ -0,0 +1,8 @@
+import { testFeature } from '../utils/feature';
+
+testFeature('dev', () => {
+  test('dev should compile correctly and responde with a 200', async() => {
+    await page.goto('http://localhost:3000');
+    expect(await page.content()).toContain('<div id="app"><div></div><div class="__cov-progress" style="background-color: rgb(0, 59, 142); opacity: 0; top: 0px; left: 0px; width: 0%; height: 2px; transition: opacity 0.6s ease 0s; position: fixed;"></div></div>');
+  });
+}, { dev: true });
