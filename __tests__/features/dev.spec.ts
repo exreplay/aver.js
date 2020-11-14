4287786d67f8a9c89569769075650d591661095a
Florian Weber <fl-weber@t-online.de>
2020-11-14 11:28:58 +0100

    refactor: added 404 check

--- __tests__/features/dev.spec.ts
+++ __tests__/features/dev.spec.ts
@@ -1,8 +1,14 @@
 import { testFeature } from '../utils/feature';
 
 testFeature('dev', () => {
-  test('dev should compile correctly and responde with a 200', async() => {
+  test('should compile correctly and responde with a 200', async() => {
     await page.goto('http://localhost:3000');
     expect(await page.content()).toContain('<div id="app"><div></div><div class="__cov-progress" style="background-color: rgb(0, 59, 142); opacity: 0; top: 0px; left: 0px; width: 0%; height: 2px; transition: opacity 0.6s ease 0s; position: fixed;"></div></div>');
+  });
+
+  test('should responde with a 404 correctly', async() => {
+    const response = await page.goto('http://localhost:3000/404');
+    console.log(response?.headers());
+    expect(response?.status()).toBe(404);
   });
 }, { dev: true });
