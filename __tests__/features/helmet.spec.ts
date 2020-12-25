6d56db279070013a8d83658816750840c4d2b01e
Florian Weber <fl-weber@t-online.de>
2020-12-25 09:48:55 +0100

    test: removed test sequencer

--- __tests__/features/helmet.spec.ts
+++ __tests__/features/helmet.spec.ts
@@ -1,14 +1,10 @@
 import { testFeature } from '../utils/feature';
 
-testFeature(
-  'helmet',
-  () => {
-    test('helmet should work correctly and headers should not contain referrer policy', async () => {
-      const response = await page.goto('http://localhost:3000');
-      expect(response?.headers()).not.toContain({
-        'referrer-policy': 'no-referrer'
-      });
+testFeature('helmet', () => {
+  test('helmet should work correctly and headers should not contain referrer policy', async () => {
+    const response = await page.goto('http://localhost:3000');
+    expect(response?.headers()).not.toContain({
+      'referrer-policy': 'no-referrer'
     });
-  },
-  { showConsoleLogs: true }
-);
+  });
+});
--- jest.config.js
+++ jest.config.js
@@ -11,8 +11,6 @@ const corePackages = fs
 module.exports = {
   preset: 'jest-puppeteer',
 
-  testSequencer: './testSequencer.js',
-
   clearMocks: true,
 
   expand: true,
