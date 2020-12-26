a93cf10722e853d88104ba54b5b23974666f7e78
Florian Weber <fl-weber@t-online.de>
2020-12-26 10:31:15 +0100

    chore: log error log

--- __tests__/features/csrf-disabled.spec.ts
+++ __tests__/features/csrf-disabled.spec.ts
@@ -1,10 +1,18 @@
 import { testFeature } from '../utils/feature';
+import fs from 'fs';
+import path from 'path';
 
 testFeature('csrf-disabled', () => {
   test('should not have csrf meta tag and cookie', async () => {
     await page.goto('http://localhost:3000');
     const content = await page.content();
     const cookies = await page.cookies();
+    console.log(
+      fs.readFileSync(
+        path.resolve(process.env.PROJECT_PATH, '../storage/log/error.log'),
+        'utf-8'
+      )
+    );
     expect(content).not.toContain('<meta name="csrf-token"');
     expect(content).toContain(
       `<span>Instance variable: </span><span>Axios default: </span>`
