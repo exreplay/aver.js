87effb8befb48b220cc6d685cd148a1aadbcf603
Florian Weber <fl-weber@t-online.de>
2020-12-28 13:38:17 +0100

    test: added test for webpack config hooks

---
+++ __tests__/features/webpack-config-hooks.spec.ts
@@ -0,0 +1,9 @@
+import { testFeature } from '../utils/feature';
+
+testFeature('webpack-config-hooks', () => {
+  /**
+   * Actual test is running inside aver-config file
+   */
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  test('should run hooks correctly', () => {});
+});
