5ebff09ee195a0d85095011b3ef202018adf76c9
Florian Weber <fl-weber@t-online.de>
2020-12-30 15:39:22 +0100

    chore: disable options

--- __tests__/features/service-worker.spec.ts
+++ __tests__/features/service-worker.spec.ts
@@ -2,50 +2,42 @@ import { aver, testFeature } from '../utils/feature';
 import fs from 'fs';
 import path from 'path';
 
-testFeature(
-  'service-worker',
-  currentDir => {
-    const serviceWorkerPath = path.resolve(
-      currentDir,
-      './dist/service-worker.js'
-    );
+testFeature('service-worker', currentDir => {
+  const serviceWorkerPath = path.resolve(
+    currentDir,
+    './dist/service-worker.js'
+  );
 
-    test('should generate service worker file', async () => {
-      expect(fs.existsSync(serviceWorkerPath)).toBeTruthy();
+  test('should generate service worker file', async () => {
+    expect(fs.existsSync(serviceWorkerPath)).toBeTruthy();
 
-      const response = await page.goto(
-        'http://localhost:3000/service-worker.js'
-      );
-      const content = await response?.text();
-      expect(response?.status()).toBe(200);
-      expect(content).toContain('skipWaiting()');
-      expect(content).toContain('prefix:"averjs"');
-      expect(content).toContain('/^(?!\\/?api).+$/');
-      expect(content).toContain('/\\.(?:png|gif|jpg|jpeg|webp|svg)$/');
-    });
+    const response = await page.goto('http://localhost:3000/service-worker.js');
+    const content = await response?.text();
+    expect(response?.status()).toBe(200);
+    expect(content).toContain('skipWaiting()');
+    expect(content).toContain('prefix:"averjs"');
+    expect(content).toContain('/^(?!\\/?api).+$/');
+    expect(content).toContain('/\\.(?:png|gif|jpg|jpeg|webp|svg)$/');
+  });
 
-    test('should work with inject manifest', async () => {
-      await aver.close();
-      if (aver.config.webpack?.sw)
-        aver.config.webpack.sw = {
-          mode: 'InjectManifest',
-          swSrc: './service-worker.js',
-          exclude: [/\.(?:png|gif|jpg|jpeg|webp)$/, /\.mp4$/, /\.pdf$/]
-        };
+  test('should work with inject manifest', async () => {
+    await aver.close();
+    if (aver.config.webpack?.sw)
+      aver.config.webpack.sw = {
+        mode: 'InjectManifest',
+        swSrc: './service-worker.js',
+        exclude: [/\.(?:png|gif|jpg|jpeg|webp)$/, /\.mp4$/, /\.pdf$/]
+      };
 
-      await aver.build({});
-      await aver.run();
+    await aver.build({});
+    await aver.run();
 
-      expect(fs.existsSync(serviceWorkerPath)).toBeTruthy();
+    expect(fs.existsSync(serviceWorkerPath)).toBeTruthy();
 
-      const response = await page.goto(
-        'http://localhost:3000/service-worker.js'
-      );
-      const content = await response?.text();
-      expect(response?.status()).toBe(200);
-      expect(content).toContain('skipWaiting()');
-      expect(content).toContain(`prefix:"inject_manifest"`);
-    });
-  },
-  { keepDist: true }
-);
+    const response = await page.goto('http://localhost:3000/service-worker.js');
+    const content = await response?.text();
+    expect(response?.status()).toBe(200);
+    expect(content).toContain('skipWaiting()');
+    expect(content).toContain(`prefix:"inject_manifest"`);
+  });
+});
