aec873c20e09f1850477ab47d80db0bffd8ee4e2
Florian Weber <fl-weber@t-online.de>
2021-01-01 15:24:57 +0100

    test: changed dotenv test

--- __tests__/features/dotenv.spec.ts
+++ __tests__/features/dotenv.spec.ts
@@ -7,20 +7,20 @@ testFeature('dotenv', currentDir => {
   const dotenvPath = path.resolve(currentDir, './.env');
 
   afterAll(() => {
-    fs.writeFileSync(dotenvPath, "TEST='hello world'");
+    fs.writeFileSync(dotenvPath, "DOTENV_TEST='hello world'");
   });
 
   test('should load and update env variables', async () => {
     await page.goto('http://localhost:3000/');
-    expect(process.env.TEST).toBe('hello world');
+    expect(process.env.DOTENV_TEST).toBe('hello world');
 
     await aver.close();
 
-    fs.writeFileSync(dotenvPath, "TEST='hello new world'");
+    fs.writeFileSync(dotenvPath, "DOTENV_TEST='hello new world'");
 
     let newAver: Aver | null = new Aver();
 
-    expect(process.env.TEST).toBe('hello new world');
+    expect(process.env.DOTENV_TEST).toBe('hello new world');
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     newAver = null;
