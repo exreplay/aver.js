473d4ec62cb0f1eed3bd0bf7ebebbb5f62c2c861
Florian Weber <fl-weber@t-online.de>
2021-01-02 08:23:18 +0100

    chore: remove show console logs

--- __tests__/features/async-data.spec.ts
+++ __tests__/features/async-data.spec.ts
@@ -1,31 +1,27 @@
 import { testFeature } from '../utils/feature';
 
-testFeature(
-  'async-data',
-  () => {
-    test('should fetch data before rendering component on page change', async () => {
-      await page.goto('http://localhost:3000');
-      await (await page.$('a'))?.click();
-      const watchDog = page.waitForFunction('window.status === "ready"');
-      await watchDog;
-      expect(await page.content()).toContain(
-        '<div id="app"><div>some async data</div></div>'
-      );
-    });
+testFeature('async-data', () => {
+  test('should fetch data before rendering component on page change', async () => {
+    await page.goto('http://localhost:3000');
+    await (await page.$('a'))?.click();
+    const watchDog = page.waitForFunction('window.status === "ready"');
+    await watchDog;
+    expect(await page.content()).toContain(
+      '<div id="app"><div>some async data</div></div>'
+    );
+  });
 
-    test('should fetch data before ssr', async () => {
-      const response = await page.goto('http://localhost:3000/test');
-      expect(await response?.text()).toContain(
-        '<div id="app" data-server-rendered="true"><div>some async data</div></div>'
-      );
-    });
+  test('should fetch data before ssr', async () => {
+    const response = await page.goto('http://localhost:3000/test');
+    expect(await response?.text()).toContain(
+      '<div id="app" data-server-rendered="true"><div>some async data</div></div>'
+    );
+  });
 
-    test('should have the asyncData class hook registered', async () => {
-      const response = await page.goto('http://localhost:3000/test-class');
-      expect(await response?.text()).toContain(
-        '<div id="app" data-server-rendered="true"><div>some async data</div></div>'
-      );
-    });
-  },
-  { showConsoleLogs: true }
-);
+  test('should have the asyncData class hook registered', async () => {
+    const response = await page.goto('http://localhost:3000/test-class');
+    expect(await response?.text()).toContain(
+      '<div id="app" data-server-rendered="true"><div>some async data</div></div>'
+    );
+  });
+});
