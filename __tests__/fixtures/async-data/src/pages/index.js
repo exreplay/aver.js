b20a5ce3c9b7511e1f9614f1c26b5fafd01e8c7e
Florian Weber <fl-weber@t-online.de>
2021-01-02 08:22:56 +0100

    test: check for asyncData class hook

--- __tests__/fixtures/async-data/src/pages/index.js
+++ __tests__/fixtures/async-data/src/pages/index.js
@@ -8,5 +8,10 @@ export default [
     path: '/test',
     name: 'test',
     component: () => import('./Test.vue')
+  },
+  {
+    path: '/test-class',
+    name: 'test-class',
+    component: () => import('./TestClass.vue')
   }
 ];
