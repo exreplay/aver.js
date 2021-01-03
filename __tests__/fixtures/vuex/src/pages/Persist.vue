109dcd74c5b5df24f9d1e83a0342c4be65e5558d
Florian Weber <fl-weber@t-online.de>
2021-01-03 10:14:32 +0100

    test: added test for vuex

---
+++ __tests__/fixtures/vuex/src/pages/Persist.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ persist }}<button @click="changePersist"></button></div>
+</template>
+
+<script>
+import { Vue, Component } from 'vue-property-decorator';
+import { TestModule } from './vuex/TestStore';
+
+@Component
+export default class Persist extends Vue {
+  get persist() {
+    return TestModule.persist;
+  }
+
+  set persist(val) {
+    TestModule.persist = val;
+  }
+
+  changePersist() {
+    this.persist = 'changed';
+  }
+}
+</script>
--- __tests__/utils/feature.ts
+++ __tests__/utils/feature.ts
@@ -109,6 +109,7 @@ export function testFeature(
         consola.clear();
         consola.resume();
       }
+      consola.restoreAll();
     });
 
     afterEach(async () => {
