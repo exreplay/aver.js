46fe1ce9aa5b8a0ec2c1aa338099c57b26c58f17
Florian Weber <fl-weber@t-online.de>
2021-01-05 18:20:14 +0100

    fix: fixed global types for core package (#309)
    * fix: fixed global types for core package
    
    * fix: import global

--- packages/@averjs/core/lib/global.ts
+++ packages/@averjs/core/lib/global.ts
@@ -1,8 +1,9 @@
-/* eslint-disable @typescript-eslint/no-namespace */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+/* eslint-disable @typescript-eslint/no-namespace, @typescript-eslint/no-unused-vars */
+/* concat start */
 declare namespace Express {
   export interface Request {
     id?: string;
     error?: string;
   }
 }
+/* concat end */
