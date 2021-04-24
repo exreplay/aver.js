4df949a89e90a42c233e31db4faa423249d8ba8d
Florian Weber <f.weber@ppm-vi.de>
2021-04-24 13:35:56 +0200

    fix: enable async data tests

--- __tests__/features/async-data.spec.ts
+++ __tests__/features/async-data.spec.ts
@@ -2,56 +2,56 @@ import fs from 'fs';
 import path from 'path';
 import { testFeature } from '../utils/feature';
 
-// testFeature('async-data', () => {
-//   test('should fetch data before rendering component on page change', async () => {
-//     const response = await page.goto('http://localhost:3000');
-//     expect(await response?.text()).toContain(
-//       '<div id="app" data-server-rendered="true"><div>app.vue</div><div><a href="/test">ssr</a></div></div>'
-//     );
-//     expect(await page.content()).toContain(
-//       '<div id="app"><div>app.vue</div><div><a href="/test" class="">ssr</a></div></div>'
-//     );
+testFeature('async-data', () => {
+  test('should fetch data before rendering component on page change', async () => {
+    const response = await page.goto('http://localhost:3000');
+    expect(await response?.text()).toContain(
+      '<div id="app" data-server-rendered="true"><div>app.vue</div><div><a href="/test">ssr</a></div></div>'
+    );
+    expect(await page.content()).toContain(
+      '<div id="app"><div>app.vue</div><div><a href="/test" class="">ssr</a></div></div>'
+    );
 
-//     await (await page.$('a'))?.click();
-//     const watchDog = page.waitForFunction('window.status === "ready"');
-//     await watchDog;
-//     expect(await page.content()).toContain(
-//       '<div id="app"><div>app.vue</div><div><div>some async data</div><!----></div></div>'
-//     );
-//   });
+    await (await page.$('a'))?.click();
+    const watchDog = page.waitForFunction('window.status === "ready"');
+    await watchDog;
+    expect(await page.content()).toContain(
+      '<div id="app"><div>app.vue</div><div><div>some async data</div><!----></div></div>'
+    );
+  });
 
-//   test('should fetch data before ssr', async () => {
-//     const response = await page.goto('http://localhost:3000/test');
-//     expect(await response?.text()).toContain(
-//       '<div id="app" data-server-rendered="true"><div>app.vue</div><div><div>some async data</div><!----></div></div>'
-//     );
-//   });
+  test('should fetch data before ssr', async () => {
+    const response = await page.goto('http://localhost:3000/test');
+    expect(await response?.text()).toContain(
+      '<div id="app" data-server-rendered="true"><div>app.vue</div><div><div>some async data</div><!----></div></div>'
+    );
+  });
 
-//   test('should fetch data before ssr', async () => {
-//     const response = await page.goto('http://localhost:3000/test/testchild');
-//     const text = await response?.text();
-//     const content = await page.content();
-//     expect(text).toContain(
-//       '<div id="app" data-server-rendered="true"><div>app.vue</div><div><div>some async data</div><div>hello from testchild</div></div></div>'
-//     );
-//     expect(text).toContain(
-//       '<script>window.__AVER_STATE__={asyncData:{app:{data:"app.vue"}},data:[null,{data:"hello from testchild"}]}</script>'
-//     );
-//     expect(content).toContain(
-//       '<div id="app"><div>app.vue</div><div><div>some async data</div><div>hello from testchild</div></div></div>'
-//     );
-//     expect(content).toContain(
-//       '<script>window.__AVER_STATE__={asyncData:{app:{data:"app.vue"}},data:[null,{data:"hello from testchild"}]}</script>'
-//     );
-//   });
+  test('should fetch data before ssr', async () => {
+    const response = await page.goto('http://localhost:3000/test/testchild');
+    const text = await response?.text();
+    const content = await page.content();
+    expect(text).toContain(
+      '<div id="app" data-server-rendered="true"><div>app.vue</div><div><div>some async data</div><div>hello from testchild</div></div></div>'
+    );
+    expect(text).toContain(
+      '<script>window.__AVER_STATE__={asyncData:{app:{data:"app.vue"}},data:[null,{data:"hello from testchild"}]}</script>'
+    );
+    expect(content).toContain(
+      '<div id="app"><div>app.vue</div><div><div>some async data</div><div>hello from testchild</div></div></div>'
+    );
+    expect(content).toContain(
+      '<script>window.__AVER_STATE__={asyncData:{app:{data:"app.vue"}},data:[null,{data:"hello from testchild"}]}</script>'
+    );
+  });
 
-//   test('should have the asyncData class hook registered', async () => {
-//     const response = await page.goto('http://localhost:3000/test-class');
-//     expect(await response?.text()).toContain(
-//       '<div id="app" data-server-rendered="true"><div>app.vue</div><div>some async data</div></div>'
-//     );
-//   });
-// });
+  test('should have the asyncData class hook registered', async () => {
+    const response = await page.goto('http://localhost:3000/test-class');
+    expect(await response?.text()).toContain(
+      '<div id="app" data-server-rendered="true"><div>app.vue</div><div>some async data</div></div>'
+    );
+  });
+});
 
 testFeature(
   'async-data-dev',
